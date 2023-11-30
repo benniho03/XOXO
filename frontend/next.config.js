16dec1ee0fbfe7765815680eb8d9900de032ea6a
benniho03 <ben.holderle.21@lehre.mosbach.dhbw.de>
2023-11-30 22:10:59 +0100

    added prod ENV

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,9 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   env: {
-    // WS_URL: "wss://xoxo-backend.onrender.com/",
-    WS_URL: "ws://localhost:8080/",
+    WS_URL: process.env.NODE_ENV === 'production' ? "wss://xoxo-backend.onrender.com/" : "ws://localhost:8080/",
   },
 }
 
+
+
 module.exports = nextConfig
+
